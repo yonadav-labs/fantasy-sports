a757a4ca5888e3e54d0a192c34b6d688bfe55bb8
IT-corridor <jason.5001001@gmail.com>
2018-12-01 02:04:29 +0530

    add redirect

--- scripts/roto.py
+++ scripts/roto.py
@@ -22,22 +22,22 @@ def get_players(data_source):
 
         fields = ['first_name', 'last_name', 'minutes', 'money_line', 
                   'over_under', 'point_spread', 'position', 'proj_ceiling', 'opponent',
-                  'proj_custom', 'proj_floor', 'proj_original', 'proj_points', 'proj_rotowire', 
+                  'proj_custom', 'proj_floor', 'proj_original', 'proj_rotowire', 
                   'proj_site', 'proj_third_party_one', 'proj_third_party_two', 'actual_position', 
-                  'salary', 'salary_custom', 'salary_original', 'team', 'team_points', 'value']
+                  'salary', 'salary_custom', 'salary_original', 'team', 'team_points', 'value'] # 'proj_points'
         print data_source, len(players)
         for ii in players:
             defaults = { key: str(ii[key]).replace(',', '') for key in fields }
-            defaults['proj_points'] = float(ii['proj_points']) + random.randrange(-20, 20) / 10.0
-            if defaults['proj_points'] <= 0:
-                defaults['proj_points'] = float(ii['proj_points'])
+            # defaults['proj_points'] = float(ii['proj_points']) + random.randrange(-20, 20) / 10.0
+            # if defaults['proj_points'] <= 0:
+            #     defaults['proj_points'] = float(ii['proj_points'])
             defaults['play_today'] = True
 
             defaults['injury'] = html2text.html2text(ii['injury']).strip()
             obj = Player.objects.update_or_create(uid=ii['id'], data_source=data_source,
                                                   defaults=defaults)
     except:
-        pass
+        print("*** some thing is wrong ***")
 
 if __name__ == "__main__":
     Player.objects.all().update(play_today=False)
